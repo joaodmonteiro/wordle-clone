fa32a0f0d9dd8c25c93cc4f993fa7804ab09c2f4
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-07-12 11:28:38 +0100

    submit guesses

--- pages/index.js
+++ pages/index.js
@@ -7,22 +7,23 @@ const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
 
 export default function Home({ solution, emptyCells }) {
   const [numberOfTries, setNumberOfTries] = useState(0);
-  const [guesses, setGuesses] = useState(["WORLD", "FIRES"]);
+  const [guesses, setGuesses] = useState([]);
   const [cells, setCells] = useState([...emptyCells]);
-  const [currentWord, setCurrentWord] = useState("");
+  const [currentWord, setCurrentWord] = useState([]);
 
   const handleKeyDown = (event) => {
     if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
       if (currentWord.length < 5) {
-        const newWord = currentWord + event.key;
-        newWord = newWord.toUpperCase();
+        const newWord = [
+          ...currentWord,
+          { letter: event.key.toUpperCase(), color: null },
+        ];
         setCurrentWord(newWord);
         updateCells(newWord);
       }
     } else if (event.key === "Backspace") {
       if (currentWord.length > 0) {
         const newWord = currentWord.slice(0, -1);
-        newWord = newWord.toUpperCase();
         setCurrentWord(newWord);
         updateCells(newWord);
       }
@@ -35,38 +36,68 @@ export default function Home({ solution, emptyCells }) {
 
   const updateCells = (word) => {
     let newCells = [];
-    const wordArray = word.split("");
 
     if (guesses.length > 0) {
       guesses.forEach((guess) => {
-        newCells = newCells.concat(guess.split(""));
+        guess.forEach((char) => {
+          newCells.push({ letter: char.letter, color: char.color });
+        });
       });
-      newCells = newCells.concat(wordArray);
+      newCells = [...newCells, ...word];
       newCells = fillRestOfArray(newCells);
       setCells(newCells);
     } else {
-      newCells = newCells.concat(wordArray);
+      newCells = [...newCells, ...word];
       newCells = fillRestOfArray(newCells);
       setCells(newCells);
-      console.log(cells);
-      console.log(newCells);
     }
+
+    console.log(cells);
+
+    console.log("current word: ");
+    console.log(currentWord);
   };
 
   const fillRestOfArray = (array) => {
     const cellsNeeded = 30 - array.length;
-    const newArray = new Array(cellsNeeded).fill("");
+    const newArray = new Array(cellsNeeded).fill({ letter: null, color: null });
 
     return array.concat(newArray);
   };
 
   const submitGuess = (guess) => {
-    console.log(guess);
-    console.log(solution);
-
-    if (guess === solution) {
-      alert("you won!");
+    let newGuess = [];
+    for (let i = 0; i < guess.length; i++) {
+      for (let j = 0; j < solution.split("").length; j++) {
+        if (guess[i].letter === solution.split("")[j]) {
+          if (i === j) {
+            newGuess.push({
+              letter: guess[i].letter,
+              color: "green",
+            });
+            break;
+          } else {
+            newGuess.push({
+              letter: guess[i].letter,
+              color: "yellow",
+            });
+            break;
+          }
+        } else {
+          if (j === solution.split("").length - 1) {
+            newGuess.push({
+              letter: guess[i].letter,
+              color: "grey",
+            });
+          }
+        }
+      }
     }
+
+    setGuesses([...guesses, newGuess]);
+    updateCells(newGuess);
+    setCurrentWord([]);
+    console.log(newGuess);
     // Check if word is correct
     // if it is, end game
     //if its not
@@ -79,13 +110,27 @@ export default function Home({ solution, emptyCells }) {
 
     if (guesses.length > 0) {
       guesses.forEach((guess) => {
-        newCells = newCells.concat(guess.split(""));
+        guess.forEach((char) => {
+          newCells.push({ letter: char.letter, color: char.color });
+        });
       });
 
       newCells = fillRestOfArray(newCells);
       setCells(newCells);
     }
   }, []);
+
+  let grey = {
+    backgroundColor: "grey",
+  };
+
+  let green = {
+    backgroundColor: "green",
+  };
+
+  let yellow = {
+    backgroundColor: "yellow",
+  };
 
   return (
     <div className={styles.container}>
@@ -103,13 +148,17 @@ export default function Home({ solution, emptyCells }) {
         className={styles.board_container}
         tabIndex={0}
         onKeyDown={handleKeyDown}>
-        {cells.map((cell) => (
-          <div className={styles.cell}>{cell}</div>
+        {cells.map((cell, index) => (
+          <div
+            key={index}
+            className={styles.cell}
+            style={{ backgroundColor: cell.color }}>
+            {cell.letter}
+          </div>
         ))}
       </div>
 
       <p>Solution: {solution}</p>
-      <p>Current word: {currentWord}</p>
     </div>
   );
 }
@@ -121,7 +170,12 @@ export async function getServerSideProps(context) {
 
   const emptyCells = [];
   emptyCells.length = 30;
-  emptyCells.fill("");
+  emptyCells.fill({
+    letter: null,
+    color: null,
+  });
+
+  console.log(emptyCells);
 
   return {
     props: { solution, emptyCells }, // will be passed to the page component as props
